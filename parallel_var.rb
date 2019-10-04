41f54d59de35f43f274dc3f6605ee6c2b6029503
Sebastian <sebastian@szumigalski.com>
2019-10-04 23:06:23 +0800

    more examples

---
+++ parallel_var.rb
@@ -0,0 +1,22 @@
+a = 10 
+b = 20
+c = 30
+
+p a, b, c
+puts a, b, c
+
+#better syntex
+a, b , c = 40, 50, 60 
+
+p a, b, c
+puts
+puts
+
+a = 1
+b = 2
+p a, b
+puts
+# swaping numbers
+a, b = b, a
+
+p a, b
---
+++ variables.rb
@@ -0,0 +1,10 @@
+name = "Sebastian"
+last_name = "Szumigalski"
+handsome = true
+age = 40 + 4
+
+puts name
+puts last_name
+puts name +" " + last_name
+puts handsome
+puts age
