264292c17915e0684e0990abdfa8459ca114be55
Sebastian <sebastian@szumigalski.com>
2019-10-05 21:16:42 +0800

    new rb files

---
+++ contstrants.rb
@@ -0,0 +1,19 @@
+age = 25
+p age 
+
+age = 30
+p age
+
+NAME = "Sebastian"
+
+PI = "3.14159"
+
+p NAME
+p PI
+
+
+p "My name is #{NAME}"
+
+#shouldn't be change 
+NAME = "jeff"
+p NAME
---
+++ intro_to_object.rb
@@ -0,0 +1,13 @@
+p "hello world".length
+
+p "Hello World".upcase
+
+p "HELLO WORLD".downcase
+
+foo = "hello world"
+
+p foo.length
+
+p 10.next
+
+puts "hello World".inspect
